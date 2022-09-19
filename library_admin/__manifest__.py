e843ca5c095736e0118743d864efbd4753b3bc39
aosoriod <aosoriod@gmail.com>
2022-09-19 19:08:59 +0000

    [ADD] Basic Module

---
+++ library_admin/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Libray Admin',
+    'summary': '''Test app library administration''',
+    'description': ''',
+        Manage library:
+        - Libro
+    ''',
+    'author': 'Yelitza Suniaga',
+    'website': 'https://www.novapos.net',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+    
+}
